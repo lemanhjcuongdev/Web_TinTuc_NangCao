b3214ce6a9ce46c1618c900165aa360914a4bbff
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-03-29 21:02:40 +0700

    login and logout (lưu cookie và xóa cookie)

--- login.aspx.cs
+++ login.aspx.cs
@@ -4,14 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BTL_Web_TinTuc_NangCao
 {
     public partial class login : System.Web.UI.Page
     {
+        string cnnstr = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string name = Request.QueryString["name"];
+                string pass = Request.QueryString["pass"];
 
+               /* if (name == "admin" && pass == "admin")
+                {
+                    Session["admin"] = name;
+                    Session["name"] = name;
+                    Session["login"] = true;
+                }*/
+                if (name!=null && pass != null)
+                {
+                    try
+                    {
+                            using (SqlConnection cnn = new SqlConnection(cnnstr))
+                            {
+                                using (SqlCommand cmd = new SqlCommand())
+                                {
+                                    cmd.Connection = cnn;
+                                    cnn.Open();
+                                    cmd.CommandType = CommandType.Text;
+                                    //cmd.CommandText = "select * from tblUser";
+                                    cmd.CommandText = "select * from tblUser where sTenTaiKhoan = '" + name + "'and sMatKhau='" + pass + "'";
+                                    SqlDataReader reader = cmd.ExecuteReader();
+                                    if (reader.Read())
+                                    {
+                                    /* Session["name"] = name;
+                                     Session["login"] = true;*/
+
+                                    HttpCookie user = new HttpCookie("user");
+                                    user.Value = name;
+                                    user.Expires = DateTime.Now.AddSeconds(10);
+                                    HttpContext.Current.Response.Cookies.Add(user);
+
+                                    Response.Redirect("trangchu.aspx");
+                                    }
+                                    else
+                                    {
+                                     error.InnerHtml = "thông tin đăng nhập không chính xác!";
+                                    }
+                                    cnn.Close();
+                                }
+                            }
+
+                    }   
+                    catch (Exception ex)
+                    {
+                        Response.Write("ERORR:" + ex.Message);
+                    }
+                }
+            }
         }
     }
 }
---
+++ logout.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BTL_Web_TinTuc_NangCao
+{
+    public partial class logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Xóa cookie với tên đã cho
+            HttpCookie cookie = new HttpCookie("user");
+            cookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(cookie);
+
+            Response.Redirect("trangchu.aspx");
+        }
+    }
+}
