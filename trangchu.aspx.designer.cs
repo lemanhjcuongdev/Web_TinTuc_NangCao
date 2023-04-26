47f0cb9ad7e8ebcc5fef575e59062b3bfa93b105
quanthht <quanvuong123456789@gmail.com>
2023-04-26 17:24:22 +0700

    push lần cuối

--- trangchu.aspx.designer.cs
+++ trangchu.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace BTL_Web_TinTuc_NangCao
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptBaoTrangChu;
-
-        /// <summary>
-        /// Repeater1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Repeater1;
     }
 }
